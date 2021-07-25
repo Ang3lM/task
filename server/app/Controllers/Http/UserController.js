e81d90f5993d58f028f665c1d170891d0a31d236
Ang3lM <angellara5a2015@gmail.com>
2021-07-25 15:58:14 -0400

    [IMP] Creacion de expoint para elimiar los projectos

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class UserController {
+
+	async login({request, auth}){
+		const {email, password} = request.all();
+		const token = await auth.attempt(email,password);
+		return token;
+
+	}
+
+	async register({request}){
+		const {username, email, password} = request.all();
+
+		await User.create({
+			username,
+			email,
+			password,
+		});
+		console.log("argumentsssssssssssssssssssssssssssss");
+		return this.login(...arguments);
+	}
+}
+
+module.exports = UserController
